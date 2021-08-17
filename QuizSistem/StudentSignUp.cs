d74ddc5364322e233794b47dad8de4771a3f48a4
AhmedHajiyev <hajiyev.ahmed01@gmail.com>
2021-08-17 13:21:18 +0400

    files added

---
+++ QuizSistem/StudentSignUp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizSistem
+{
+    public partial class StudentSignUp : Form
+    {
+        public StudentSignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            SignInForm signInForm = new SignInForm();
+            signInForm.ShowDialog();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string password = PasswordGenerator.GeneratePassword();
+
+            using (QuizSystemDbContext context = new QuizSystemDbContext())
+            {
+                var student = new Student
+                {
+                    Email = emailBox.Text,
+                    Name = usernameBox.Text,
+                    Password = password
+                };
+
+                context.Students.Add(student);
+                context.SaveChanges();
+
+
+                GmailSender.SendGmail(student.Email, student.Name, student.Password);
+
+                this.Hide();
+                SignInForm signInForm = new SignInForm();
+                signInForm.ShowDialog();
+                this.Close();
+            }
+
+            
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            usernameBox.Clear();
+            emailBox.Clear();
+        }
+    }
+}
---
+++ QuizSistem/TeacherSignUp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizSistem
+{
+    public partial class TeacherSignUp : Form
+    {
+        public TeacherSignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            SignInForm signInForm = new SignInForm();
+            signInForm.ShowDialog();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string password = PasswordGenerator.GeneratePassword();
+
+            using (QuizSystemDbContext context = new QuizSystemDbContext())
+            {
+                var teacher = new Teacher
+                {
+                    Name = usernameBox.Text,
+                    Email = emailBox.Text,
+                    Password = password
+                };
+
+                context.Teachers.Add(teacher);
+                context.SaveChanges();
+
+                GmailSender.SendGmail(teacher.Email, teacher.Name, teacher.Password);
+
+                this.Hide();
+                SignInForm signInForm = new SignInForm();
+                signInForm.ShowDialog();
+                this.Close();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            usernameBox.Clear();
+            emailBox.Clear();
+        }
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            SignInForm signInForm = new SignInForm();
+            signInForm.ShowDialog();
+            this.Close();
+        }
+    }
+}
